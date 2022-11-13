8b336b1d31e79e9bc403d469b5a04f263f43e6c2
nelsonviloriadeguzman <nelsonviloriadeguzman@gmail.com>
2022-11-13 22:10:42 +0800

    Update

--- pages/blankwithsidebar/js/script.js
+++ pages/blankwithsidebar/js/script.js
@@ -1,9 +1,36 @@
 const data = {
     div:{
-        headerTitle : "CRUD Data",
+        headerTitle : "Proof Of Payment",
         headerCloseText : "Close",
         headerCloseName : document.getElementsByClassName("header-close")[0]
-    }
+    },
+    currentTab: 'Page 1',
+    contentTitle: ['Page 1','Page 2','Page 3'],
+    contentTitleContainer: document.getElementById("content-title"),
+    contentContainer: document.getElementById("content-list"),
+    contentList: [
+
+        {
+            contentTitle : 'Page 1',
+            type: 'Type 1',
+            path: 'img/.png',
+            date: 'November 2022',
+        },
+        {
+            contentTitle : 'Page 2',
+            type: 'Type 2',
+            path: 'img/.png',
+            date: 'November 2022',
+        },
+        {
+            contentTitle : 'Page 3',
+            type: 'Type 3',
+            path: 'img/.png',
+            date: 'November 2022',
+        }
+ 
+    ],
+    
 }
 
 data.div.headerCloseName.addEventListener('click', () => {
@@ -13,3 +40,58 @@ data.div.headerCloseName.addEventListener('click', () => {
 document.title = data.div.headerTitle;
 document.getElementsByClassName("header-title")[0].innerText = data.div.headerTitle;
 document.getElementsByClassName("header-close")[0].innerText = data.div.headerCloseText;
+
+
+/************************************************
+*************** LEFT CONTENT ********************
+************************************************/
+
+function loadTableContent(currentTab){
+    data.contentContainer.innerText = currentTab
+}  
+
+function loadSidebarMenu(){
+    data.contentTitle.forEach( each => {
+
+        let li = document.createElement("li");
+        let btn = document.createElement("button");
+        btn.innerHTML = each;
+        btn.setAttribute("id", each); // to add id
+        li.appendChild(btn);
+        data.contentTitleContainer.appendChild(li);
+
+    })
+
+    document.getElementById(data.contentTitle[0]).classList.add('active');
+}
+
+document.addEventListener("click", (event) => { 
+    let targetElement = event.target
+    
+    if (data.contentTitle.includes(targetElement.id)){
+
+        data.contentTitle.forEach( each => {
+            document.getElementById(each).classList.remove('active');
+        })
+
+        document.getElementById(targetElement.id).classList.add('active');
+        data.currentTab = targetElement.id
+        loadTableContent(targetElement.id)
+    }
+});
+
+addEventListener('load', () => {
+    loadSidebarMenu()
+    loadTableContent(data.currentTab)  
+});
+
+
+
+
+
+
+
+
+
+
+
