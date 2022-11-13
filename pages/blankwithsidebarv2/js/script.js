d291439a771a9fee64928c188795f55ed8d311c6
nelsonviloriadeguzman <nelsonviloriadeguzman@gmail.com>
2022-11-13 17:33:20 +0800

    Update

--- pages/blankwithsidebarv2/js/script.js
+++ pages/blankwithsidebarv2/js/script.js
@@ -1,11 +1,35 @@
 const data = {
     div:{
-        headerTitle : "Blank Page With Side Bar",
+        headerTitle : "Proof Of Payment",
         headerCloseText : "Close",
         headerCloseName : document.getElementsByClassName("header-close")[0]
     },
-    contentList: ['Page 1','Page 2','Page 3'],
-    currentId: '',
+    currentTab: 'Page 1',
+    contentTitle: ['Page 1','Page 2','Page 3'],
+    contentTitleContainer: document.getElementById("content-title"),
+    contentContainer: document.getElementById("content-list"),
+    contentList: [
+
+        {
+            contentTitle : 'Page 1',
+            type: 'Type 1',
+            path: 'img/.png',
+            date: 'November 2022',
+        },
+        {
+            contentTitle : 'Page 2',
+            type: 'Type 2',
+            path: 'img/.png',
+            date: 'November 2022',
+        },
+        {
+            contentTitle : 'Page 3',
+            type: 'Type 3',
+            path: 'img/.png',
+            date: 'November 2022',
+        }
+ 
+    ],
     
 }
 
@@ -22,36 +46,48 @@ document.getElementsByClassName("header-close")[0].innerText = data.div.headerCl
 *************** LEFT CONTENT ********************
 ************************************************/
 
-addEventListener('load', (event) => {
-    document.getElementById(data.contentList[0]).classList.add('active');
-    document.getElementById('right-content').innerHTML = data.contentList[0]
-});
+function loadTableContent(currentTab){
+    data.contentContainer.innerText = currentTab
+}  
 
+function loadSidebarMenu(){
+    data.contentTitle.forEach( each => {
 
-let ul = document.getElementById("content-list");
+        let li = document.createElement("li");
+        let btn = document.createElement("button");
+        btn.innerHTML = each;
+        btn.setAttribute("id", each); // to add id
+        li.appendChild(btn);
+        data.contentTitleContainer.appendChild(li);
 
-for (let contentCount = 0; contentCount < data.contentList.length; ++contentCount) {
-    let li = document.createElement("li");
-    let a = document.createElement("button");
-    a.innerHTML = data.contentList[contentCount];
-    a.setAttribute("id", data.contentList[contentCount]); // to add id
-    li.appendChild(a);
-    ul.appendChild(li);
+    })
+
+    document.getElementById(data.contentTitle[0]).classList.add('active');
 }
 
-
- document.addEventListener("click", function(event){
+document.addEventListener("click", (event) => { 
     let targetElement = event.target
     
-    if (data.contentList.includes(targetElement.id)){
-        for (let contentCount = 0; contentCount < data.contentList.length; ++contentCount) {  
-            document.getElementById(data.contentList[contentCount]).classList.remove('active');
-        }
+    if (data.contentTitle.includes(targetElement.id)){
+
+        data.contentTitle.forEach( each => {
+            document.getElementById(each).classList.remove('active');
+        })
+
         document.getElementById(targetElement.id).classList.add('active');
-        data.currentId = targetElement.id
-        document.getElementById('right-content').innerHTML = targetElement.id
+        data.currentTab = targetElement.id
+        loadTableContent(targetElement.id)
     }
 });
+
+addEventListener('load', () => {
+    loadSidebarMenu()
+    loadTableContent(data.currentTab)  
+});
+
+
+
+
 
 
 
