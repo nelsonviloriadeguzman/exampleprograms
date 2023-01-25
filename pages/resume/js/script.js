fca00a870f5be9b81fa6497543a29756bad3296c
nelsonviloriadeguzman <nelsonviloriadeguzman@gmail.com>
2023-01-26 00:14:34 +0800

    New Resume Format
    New Resume Format

--- pages/resume/js/script.js
+++ pages/resume/js/script.js
@@ -14,12 +14,47 @@ new Vue({
           imgStyle:{
             border: '1px solid black'
           },
+
+          workExperience:{
+            jobPosition: `Office Staff / Programmer`,
+            companyName: `H.R.D. Singapore Pte., Ltd.`,
+            companyAddress: `Blk 3, Cavite Eco Zone II Gen. Trias, Cavite`,
+            companyContact: `(046) 476-0545 / (046) 476-0465 / (02) 857-8210`,
+            employmentDuration: `June 2018 - September 2020`,
+            workResponsibilities: [
+              `Analyze system flow and the process of all handled system`,
+              `Modify existing system for improvement`,
+              `Fixing system troubles`,
+              `Tracing of missing data or if there's data discrepancy`,
+              `Monitor server activities and performace`,
+              `Act as support for system user for their system inqueries`,
+              `Data modification for correction as request by user`,
+              `Performs data extraction as requested by user`
+            ],
+          },
+
+  
+          education:{
+            college: {
+              collegeName: `National College of Science and Technology`,
+              collegeAddress: `Amafel Building Aguinaldo Highway, Dasmariñas City, Cavite`,
+              collegeContact: `(046) 416-NCST (6278)`,
+              collegeCourse: `Bachelor of Science in Information Technology`,
+              collegeDuration: `2013 - 2017`
+
+            },
+            highschool: {
+              highschoolName: `Manalo Academy School`,
+              highschoolAddress: ` Gen. Trias, Cavite`,
+              level: `High School`,
+              highschoolDuration: `2003 - 2007`
+            },
+          },
           
           shortProfile: {
             fullName: 'Nelson V. de Guzman',
-            mobileNumber: '+63 915 7959 026',
+            mobileNumber: '+63 926 659 3763',
             emailAddress: 'nelsonviloriadeguzman@gmail.com',
-            skypeName: 'live:nelsonviloriadeguzman',
             github: 'nelsonviloriadeguzman.github.io/exampleprograms/',
             imagePath: 'img/defaultImage.png'
           },
@@ -169,160 +204,136 @@ new Vue({
           var dd = {
             content: [
                 
-                //WORK EXPERIECE
-              
-                {
-                    layout: 'noBorders',
+               //FULL NAME
+               {
+                layout: 'noBorders',
                 table: {
-                    widths:['0%','50%','50%'],
-                  body: [[	{text:''},
-                          {
-                              style: 'infoStyle',
-                                    layout: 'noBorders',
-                                table: {
-                                    widths:['*'],
-                                  body: [[{ text: `${this.shortProfile.fullName}
-                                                  ${this.shortProfile.mobileNumber}
-                                                  ${this.shortProfile.emailAddress}
-                                                  ${this.shortProfile.skypeName}
-                                                  ${this.shortProfile.github}`}]]
-                                    },
-                                  
-                          },
-                          {
-                                layout: {
-                                      hLineColor: function(i, node) {
-                                          return i === 1  ? 'black' : 'white';
-                                      },
-                                      vLineColor: function(i, node) {
-                                          return 'white';
-                                      },
-                                      paddingTop: function(i, node) { 
-                                        return 0 },
-                                      paddingBottom: function(i, node) { 
-                                        return i == 1 ? 3 : 0; 
-                                      }
-                                    },
-                                table: {
-                                    widths:['*'],
-                                  body: [[{ text: 'Work Experience', style: 'headerStyle',}],
-                                          [{ text: '\nOffice Staff / Programmer', style:"miniTitleStyle",}],
-                                          [{ text: 'H.R.D. Singapore Pte., Ltd.\nBlk 3, Cavite Eco Zone II Gen. Trias, Cavite\n(046) 476-0545 / (046) 476-0465 / (02) 857-8210\nJune 2018 - September 2020\n\n',style: 'defaultTextStyle'}]
-                                      ]
-                                    },
-                                  
-                                }
-                              ]]
-                },
+                  body: [
+                          [{ text: ' '}],
+                          [{ text: this.shortProfile.fullName ,style: 'miniTitleStyle'}],
+                          [{ text: this.shortProfile.mobileNumber ,style: 'defaultTextStyle'}],
+                          [{ text: this.shortProfile.emailAddress ,style: 'defaultTextStyle'}],
+                          [{ text: this.shortProfile.github ,style: 'defaultTextStyle'}],
+                          [{ text: ' '}]
+                        ]
+                    },
                   
                 },
 
-              //INFORMATION TECHNOLOGY SKILLS
-                {
+                //EXPERIENCE
+                 {
                   layout: 'noBorders',
                   style: 'headerStyle',
+              table: {
+                  widths:['*'],
+                body: [[{ text: 'EXPERIENCE',}]]
+              },
+                
+              },
+              // VERTICAL LINE START
+              {
+                layout: {
+                    hLineWidth: function(i, node) {
+                        return (i === 0 || i === node.table.body.length) ? 0 : 1;
+                    },
+                    vLineWidth: function(i, node) {
+                        return 0;
+                    },
+                    paddingTop: function(i, node) { return -10; },
+                    paddingBottom: function(i, node) { return -3; }
+                },
                 table: {
-                    widths:['*'],
-                  body: [[{ text: 'Information Technology Skills',}]]
+                    widths: ['*'],
+                    body: [[" "], [" "]]
                 },
-                  
-                },
-                // VERTICAL LINE START
-                {
-                  layout: {
-                      hLineWidth: function(i, node) {
-                          return (i === 0 || i === node.table.body.length) ? 0 : 1;
-                      },
-                      vLineWidth: function(i, node) {
-                          return 0;
-                      },
-                      paddingTop: function(i, node) { return -10; },
-                      paddingBottom: function(i, node) { return -3; }
+              },
+              // VERTICAL LINE END
+              {
+                  layout: 'noBorders',
+              table: {
+                body: [
+                        [{ text: ' '}],
+                        [{ text: this.workExperience.jobPosition ,style: 'miniTitleStyle'}],
+                        [{ text: this.workExperience.companyName ,style: 'defaultTextStyle'}],
+                        [{ text: this.workExperience.companyAddress ,style: 'defaultTextStyle'}],
+                        [{ text: this.workExperience.companyContact ,style: 'defaultTextStyle'}],
+                        [{ text: this.workExperience.employmentDuration ,style: 'defaultTextStyle'}],
+                        [{ text: ' '}]
+                      ]
                   },
-                  table: {
-                      widths: ['*'],
-                      body: [[" "], [" "]]
-                  },
+                
+              },
+
+              //RESPONSIBILITIES
+              { style: 'defaultTextStyle',
+                ul: this.workExperience.workResponsibilities
+              },
+              {
+                text: ' '
+              },
+
+                
+
+
+              //SKILLS
+              {
+                layout: 'noBorders',
+                style: 'headerStyle',
+              table: {
+                  widths:['*'],
+                body: [[{ text: 'SKILLS',}]]
+              },
+                
+              },
+              // VERTICAL LINE START
+              {
+                layout: {
+                    hLineWidth: function(i, node) {
+                        return (i === 0 || i === node.table.body.length) ? 0 : 1;
+                    },
+                    vLineWidth: function(i, node) {
+                        return 0;
+                    },
+                    paddingTop: function(i, node) { return -10; },
+                    paddingBottom: function(i, node) { return -3; }
                 },
-                // VERTICAL LINE END
-                {
-                    layout: 'noBorders',
                 table: {
-                    widths:['*','*','*','*','*'],
-                  body: [[{ text: '\nWeb Application Core ', style:"miniTitleStyle"},
-                          { text: '\nJavaScript Framework ', style:"miniTitleStyle"},
-                          { text: '\nCSS Framework ', style:"miniTitleStyle"},
-                          { text: '\nServer Side / Back End', style:"miniTitleStyle"},
-                          { text: '\nDatabase', style:"miniTitleStyle"},
-                          ],
-                          
-                          [{ text: "HTML\nCSS\nJavaScript\n\n", style: 'defaultTextStyle'},
-                          { text: "Vue.js\n\n", style: 'defaultTextStyle'},
-                          { text: 'Vuetify\nBootstrap\nMaterialize\n\n', style: 'defaultTextStyle'},
-                          { text: 'Node.js\nMagicXPA\nPHP\nc#\n\n', style: 'defaultTextStyle'},
-                          { text: 'MySQL\nCouchDB\nSQL Server\nMS Access\n\n', style: 'defaultTextStyle'},
-                          ],
-                          
-                          [{},{},{},{},{}]
-                          ],
-                          
+                    widths: ['*'],
+                    body: [[" "], [" "]]
                 },
-                  
+              },
+              // VERTICAL LINE END
+              {
+                  layout: 'noBorders',
+              table: {
+                  widths:['*','*','*','*'],
+                body: [[
+                        { text: '\nJavaScript Framework ', style:"skillStyle", bold: true},
+                        { text: '\nCSS Framework ', style:"skillStyle", bold: true},
+                        { text: '\nServer Side / Back End', style:"skillStyle", bold: true},
+                        { text: '\nDatabase', style:"skillStyle", bold: true},
+                        ],
+                        
+                        [
+                        { text: "Vue.js\n\n", style: 'skillStyle'},
+                        { text: 'Vuetify\nBootstrap\nMaterialize\n\n', style: 'skillStyle'},
+                        { text: 'Node.js\nMagicXPA\nPHP\nc#\n\n', style: 'skillStyle'},
+                        { text: 'MySQL\nCouchDB\nSQL Server\nMS Access\n\n', style: 'skillStyle'},
+                        ],
+                        
+                        [{},{},{},{}]
+                        ],
+                        
                 },
                 
-              //DUTIES AND RESPONSIBILITIES
-                {
-                    layout: 'noBorders',
-                    style: 'headerStyle',
-                table: {
-                    widths:['*'],
-                  body: [[{ text: 'Duties and Responsibilities',}]]
-                },
-                  
-                },
-                // VERTICAL LINE START
-                {
-                  layout: {
-                      hLineWidth: function(i, node) {
-                          return (i === 0 || i === node.table.body.length) ? 0 : 1;
-                      },
-                      vLineWidth: function(i, node) {
-                          return 0;
-                      },
-                      paddingTop: function(i, node) { return -10; },
-                      paddingBottom: function(i, node) { return -3; }
-                  },
-                  table: {
-                      widths: ['*'],
-                      body: [[" "], [" "]]
-                  },
-                },
-                // VERTICAL LINE END
-                {
-                    layout: 'noBorders',
-                table: {
-                    widths:['*','*'],
-                  body: [[{ text: '\nDaily', style:"miniTitleStyle"},
-                          { text: '\nPeriodic/Occasional', style:"miniTitleStyle"},
-                          ],
-                          
-                          [{ text: "Analyze system flow and the process of all handled system\nModify existing system for improvement\nFixing system troubles\nTracing of missing data or if there's data discrepancy\nMonitor server activities and performace\nAct as support for system user for their system inqueries\nData modification for correction as request by user\nPerforms data extraction as requested by user\n\n", style: 'defaultTextStyle'},
-                          { text: 'Update of master tables if needed as users request\nSupport and attends to co-members queries in technial aspects\nSuggest improvement for lessen manual checking\n Develop the approved requested system\nAdminister the newly developed system from trial deployment up to actual impementation\nAttend meeting with requester as part of system development and system modification if needed\nTrainer of newly hired staffs\n\n', style: 'defaultTextStyle'},
-                          ],
-                          
-                          [{},{}]
-                          ],
-                          
-                },
-                  
-                },
+              },
                 //EDUCATION
                 {
                     layout: 'noBorders',
                     style: 'headerStyle',
                 table: {
                     widths:['*'],
-                  body: [[{ text: 'Education',}]]
+                  body: [[{ text: 'EDUCATION',}]]
                 },
                   
                 },
@@ -344,15 +355,28 @@ new Vue({
                   },
                 },
                 // VERTICAL LINE END
+
+                {
+                  text: ' '
+                },
                 {
                     layout: 'noBorders',
                 table: {
                     widths:['*','*'],
-                  body: [[{ text: '\nNational College of Science and Technology', style:"miniTitleStyle"},
-                          { text: '\nManalo Academy School', style:"miniTitleStyle"},
+                  body: [[
+                          { text: this.education.college.collegeName, style:"miniTitleStyle"},
+                          { text: this.education.highschool.highschoolName, style:"miniTitleStyle"},
                           ],
-                          [{ text: 'Amafel Building Aguinaldo Highway, Dasmariñas City, Cavite \n (046) 416-NCST (6278) \n B.S in Information Technology \n 2013 - 2017', style: 'defaultTextStyle'},
-                          { text: 'Gen. Trias, Cavite \n High School \n 2003 - 2007', style: 'defaultTextStyle'},
+                          [{ text: `${ this.education.college.collegeAddress}  
+                                    ${ this.education.college.collegeContact} 
+                                    ${ this.education.college.collegeCourse}  
+                                    ${ this.education.college.collegeDuration}
+                                    `, style: 'defaultTextStyle'},
+
+                          { text: `${ this.education.highschool.highschoolAddress}  
+                                    ${ this.education.highschool.level} 
+                                    ${ this.education.highschool.highschoolDuration}
+                                    `, style: 'defaultTextStyle'},
                           ],
                           [{},{}]
                           ]
@@ -363,10 +387,10 @@ new Vue({
             ],
             styles: {
               defaultTextStyle:{
-                fontSize: 10,
+                fontSize: 11,
               },
               headerStyle: {
-                fontSize: 13,
+                fontSize: 11,
                 bold: true
               },
               miniTitleStyle: {
@@ -375,15 +399,19 @@ new Vue({
               },
               infoStyle: {
                   bold: true,
-                  fontSize: 15,
-              }
+                  fontSize: 11,
+              },
+              skillStyle:{
+                fontSize: 11,
+              },
               
             },
-            pageOrientation: 'landscape',
+            pageSize: 'A4',
+            pageOrientation: 'portrait',
             
           }
 
-          downloadOrPreview === 1 ? createPdf(dd).download('CV - Nelson V. de Guzman.pdf') : createPdf(dd).open()
+          downloadOrPreview === 1 ? createPdf(dd).download('Resume - Nelson V. de Guzman.pdf') : createPdf(dd).open()
       
         }
       }
